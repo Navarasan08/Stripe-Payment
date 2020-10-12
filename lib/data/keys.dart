6dea00a3ff9b5b67c319e681e86de09016c76d96
Navarasan <59596635+Navarasan08@users.noreply.github.com>
2020-10-12 08:29:08 +0530

    Update keys.dart

--- lib/data/keys.dart
+++ lib/data/keys.dart
@@ -1,6 +1,6 @@
 
 
 class PrivateKeys {
-  static const String stripeSecret = "sk_test_oqJte9s4jnKhHfQm1wsKi14D00rdiA33F0";
-  static const String stripePublishableKey = "pk_test_WmwukKgphK0RKNbE2Hoe3YXk006pWHkXgG";
+  static const String stripeSecret = "";
+  static const String stripePublishableKey = "";
 }
